0fff59e8d3c3f36715dcfadb1cfbc9893428372f
jo-v-cs <jvioo@yahoo.com>
2021-10-26 19:22:19 -0400

    Remove debug message

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 'use strict';
 let button = document.getElementById('ham-button');
 button.addEventListener('click', function() {
-    console.log("Click");
     let hamMenu = document.getElementById('ham-menu');
     if (hamMenu.style.display === 'none') {
         hamMenu.style.display = 'block';
