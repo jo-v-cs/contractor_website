3228343e03dd9d20f9532c546762f55896d6ff7e
jo-v-cs <jvioo@yahoo.com>
2021-11-09 18:31:10 -0500

    Configure routes

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "template.html"));
 });
 
+app.get('/request', (req, res) => {
+    res.sendFile(path.join(__dirname, "request.html"));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, "about.html"));
+});
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 })
