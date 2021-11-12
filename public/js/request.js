0d5dd3725bac36dc6fea1652fc7cd485def8a55e
jo-v-cs <jvioo@yahoo.com>
2021-11-12 11:29:47 -0500

    Process formdata for request

--- public/js/request.js
+++ public/js/request.js
@@ -3,5 +3,10 @@
 document.getElementById('order').onsubmit = function(event) {
     event.preventDefault(); // Prevent page refresh
     const formData = new FormData(document.getElementById('order'));
-    console.log(formData.get("email"));
+    let orderObj = {
+        name: formData.get("name"),
+        email: formData.get("email"),
+        comments: formData.get("comments")
+    }
+    console.log(orderObj);
 };
