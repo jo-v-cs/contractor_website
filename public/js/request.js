785e747756feac39fba77b18e4f0809f8af6e2c6
jo-v-cs <jvioo@yahoo.com>
2021-11-16 15:09:04 -0500

    Add quote values to formData Object

--- public/js/request.js
+++ public/js/request.js
@@ -1,14 +1,20 @@
 'use strict';
 
+function getQuote(numPlayers) {
+    return numPlayers * 50;
+}
+
 document.getElementById('order').onsubmit = function(event) {
     event.preventDefault(); // Prevent default behavior
     const formData = new FormData(document.getElementById('order'));
+    let quoteValue = getQuote(formData.get("numPlayers"));
     let orderObj = {
         name: formData.get("name"),
         email: formData.get("email"),
         comments: formData.get("comments"),
         genre: formData.get("genre"),
-        numPlayers: formData.get("numPlayers")
+        numPlayers: formData.get("numPlayers"),
+        quote: quoteValue
     }
     console.log(orderObj);
     axios
