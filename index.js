01ba43f5cfa4347e2795e632f56428e8a7ed10a7
jo-v-cs <jvioo@yahoo.com>
2021-11-18 12:04:17 -0500

    Removed obsolete code

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
-const fs = require('fs');
 const bodyParser = require('body-parser');
 
 
