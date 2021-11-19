33a121c7b509a435520aa5681bac0394aa8f96dd
jo-v-cs <jvioo@yahoo.com>
2021-11-19 11:00:08 -0500

    Make initDB more modular

--- indexLogic.js
+++ indexLogic.js
@@ -1,10 +1,10 @@
-function initDB(db, dbFields) {
+function initDB(db, dbFields, tableName) {
     let queryFields = [];
     for (let i = 0, n = dbFields.length; i < n; i++) {
         queryFields[i] = dbFields[i] + ' ' + 'TEXT';
     }
     return db.run(`CREATE TABLE 
-            IF NOT EXISTS orders(
+            IF NOT EXISTS ${tableName}(
                 id INTEGER PRIMARY KEY,
                 ${queryFields.toString()})`,
             (err) => {
